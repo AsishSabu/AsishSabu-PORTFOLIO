87a6f2197a94f69b7eea64e0d4977ee18d65ae1f
Asish Sabu <ashiasish1858@gmail.com>
2024-11-24 14:10:53 +0530

    initial comit

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bf5d5a8f22e634cb70d98d6e86341bb0@o4507540871118848.ingest.us.sentry.io/4508342964977664",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
---
+++ tailwindcss-flattenColorPalette.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module 'tailwindcss/lib/util/flattenColorPalette.js' {
+  const flattenColorPalette: (colors: Record<string, any>) => Record<string, any>;
+  export default flattenColorPalette;
+}
